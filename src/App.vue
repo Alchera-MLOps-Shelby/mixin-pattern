8c5b59eec651197b71af89324fa105dc0e4f80a4
Seong Kyu Sohn <nullterra180@gmail.com>
2023-05-25 15:48:54 +0900

    chore: change config and add content to exemplify JSX Pattern

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,68 @@
 <template>
-  <div>
-    <click-counter />
-    <hr />
-    <hover-counter />
+  <div id="app">
+    <div class="area-top">
+      <input
+        autofocus
+        type="text"
+        v-model="textInput"
+        style="margin-right: 4px"
+        @keyup.enter="addMessage()"
+      />
+      <button @click="addMessage()">Send</button>
+    </div>
+    <div class="area-bottom">
+      <JSXComponent :messages="messages" />
+      <RegularComponent :messages="messages" />
+    </div>
   </div>
 </template>
 
 <script>
-import ClickCounter from "@/components/ClickCounter.vue";
-import HoverCounter from "@/components/HoverCounter.vue";
+import moment from "moment";
+import JSXComponent from "@/components/JSXComponent";
+import RegularComponent from "@/components/RegularComponent";
 
 export default {
   name: "App",
-  components: {
-    ClickCounter,
-    HoverCounter,
+  components: { JSXComponent, RegularComponent },
+  data() {
+    return {
+      textInput: "",
+      messages: [
+        {
+          text: "Hello there!",
+          fromUser: false,
+          timestamp: "10:01 AM",
+        },
+        {
+          text: "Hey! How can I help you?",
+          fromUser: true,
+          timestamp: "10:05 AM",
+        },
+      ],
+    };
+  },
+  methods: {
+    addMessage() {
+      if (this.textInput !== "") {
+        this.messages.push({
+          text: this.textInput,
+          fromUser: true,
+          timestamp: moment().format("hh:mm a"),
+        });
+        this.textInput = "";
+      }
+    },
   },
 };
 </script>
 
 <style>
+html,
+body {
+  height: 100%;
+  width: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,5 +70,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  height: 100%;
+  width: 100%;
+}
+
+.area-top {
+  margin-top: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.area-bottom {
+  height: 500px;
+  width: 100%;
+  display: flex;
 }
 </style>
---
+++ src/components/RegularComponent.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container">
+    <h1>Welcome to the Template Chatbot Playground!</h1>
+    <div class="chatbox" v-for="(message, index) in messages" :key="index">
+      <div class="message" :class="{ user: message.fromUser }" :key="index">
+        <div class="avatar">
+          <img src="@/assets/logo.png" alt="Avatar" />
+        </div>
+        <div class="content">
+          <p>{{ message.text }}</p>
+          <span class="timestamp">{{ message.timestamp }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    messages: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.chatbox {
+  border: 1px solid #ccc;
+  padding: 20px;
+  margin-bottom: 10px;
+}
+
+.message {
+  display: flex;
+  margin-bottom: 10px;
+}
+
+.message.user {
+  justify-content: flex-end;
+}
+
+.avatar {
+  width: 50px;
+  height: 50px;
+  margin-right: 10px;
+  overflow: hidden;
+  border-radius: 50%;
+}
+
+.avatar img {
+  width: 100%;
+  height: auto;
+}
+
+.content {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.timestamp {
+  color: #999;
+  font-size: 12px;
+}
+</style>
